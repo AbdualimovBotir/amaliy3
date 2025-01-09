cb4994cba8008928265799f547592c016db6ef33
Botir <117967765+AbdualimovBotir@users.noreply.github.com>
2025-01-09 15:14:21 +0500

    Update app.py
    1

--- App/app.py
+++ App/app.py
@@ -3,14 +3,16 @@ import pickle
 import string
 from nltk.corpus import stopwords
 import nltk
-import os
-os.chdir(r"C:\Users\Rajdeep Shil\ML\Projects\SMS_SPAM_DETECTION\App")
-
 from nltk.stem.porter import PorterStemmer
 
+# Initialize the PorterStemmer
 ps = PorterStemmer()
 
+# Download the NLTK stopwords if not already downloaded
+nltk.download('punkt')
+nltk.download('stopwords')
 
+# Function to preprocess and transform text
 def transform_text(text):
     text = text.lower()
     text = nltk.word_tokenize(text)
@@ -35,24 +37,27 @@ def transform_text(text):
 
     return " ".join(y)
 
-
+# Load the vectorizer and model from the current directory
 tfidf = pickle.load(open('vectorizer2.pkl','rb'))
 model = pickle.load(open('model2.pkl','rb'))
 
+# Streamlit app interface
 st.title("Email/SMS Spam Classifier")
 
 input_sms = st.text_area("Enter the message")
 
 if st.button('Predict'):
-
-    # 1. preprocess
+    # 1. Preprocess the input text
     transformed_sms = transform_text(input_sms)
-    # 2. vectorize
+    
+    # 2. Vectorize the text
     vector_input = tfidf.transform([transformed_sms])
-    # 3. predict
+    
+    # 3. Predict the class
     result = model.predict(vector_input)[0]
-    # 4. Display
+    
+    # 4. Display the result
     if result == 1:
         st.header("Spam")
-    elif result==0:
+    elif result == 0:
         st.header("Not Spam")
